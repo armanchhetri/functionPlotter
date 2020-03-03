1b1fe459d84aef679389d60daea34c01a244f451
amnino <66.amang@gmail.com>
2020-03-03 13:10:04 +0545

    imgui front end added

--- sem5/src/main.cpp
+++ sem5/src/main.cpp
@@ -2,6 +2,9 @@
 
 #include <GLFW/glfw3.h>
 
+#include "imgui.h"
+#include "imgui_impl_glfw_gl3.h"
+
 //Mathematical libraries
 #include "Utilities/GLM/glm/glm.hpp"
 #include "Utilities/GLm/glm/gtc/matrix_transform.hpp"
@@ -563,6 +566,14 @@ int main(void)
 	glEnable(GL_DEBUG_OUTPUT);
 	glDebugMessageCallback((GLDEBUGPROC)OpenGLDebugCallback, 0);
 	/* Loop until the user closes the window */
+	ImGui::CreateContext();
+	ImGui_ImplGlfwGL3_Init(window, true);
+
+	ImGui::StyleColorsDark();
+	bool show_demo_window = true;
+	bool show_another_window = false;
+	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
@@ -570,6 +581,9 @@ int main(void)
 		float currentFrame = glfwGetTime();
 		delTime = currentFrame - lastFrame;
 		lastFrame = currentFrame;
+
+		//imguinewframe
+		ImGui_ImplGlfwGL3_NewFrame();
 
 		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -685,12 +699,38 @@ int main(void)
 		//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 		
 
+		//imguiwindow
+		{
+			static float f = 0.0f;
+			static int counter = 0;
+			ImGui::Text("Hello, world!");                           // Display some text (you can use a format string too)
+			ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
+			ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+			ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our windows open/close state
+			ImGui::Checkbox("Another Window", &show_another_window);
+
+			if (ImGui::Button("Button"))                            // Buttons return true when clicked (NB: most widgets return true when edited/activated)
+				counter++;
+			ImGui::SameLine();
+			ImGui::Text("counter = %d", counter);
+
+			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+		}
+		//imguirender
+		ImGui::Render();
+		ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
+
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
 
 		/* Poll for and process events */
 		glfwPollEvents();
 	}
+
+	//imguiterminate
+	ImGui_ImplGlfwGL3_Shutdown();
+	ImGui::DestroyContext();
 
 	glfwTerminate();
 //	delete[] g1index;
