122eab0fd47ffafd22dd2cd1179e79994c09e027
amnino <66.amang@gmail.com>
2020-03-04 08:37:47 +0545

    updated gui

--- sem5/src/main.cpp
+++ sem5/src/main.cpp
@@ -379,6 +379,8 @@ int main(void)
 	bool show_another_window = false;
 	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
+	//function for option choosing
+
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
@@ -524,19 +526,28 @@ int main(void)
 
 		//imguiwindow
 		{
+			const char* listbox_items[] = { "(x + y)*0.2", "tem * 0.008", "(sxn(d) / d)*10", "sxn(x*x + y * y)", "abs(x) - abs(y)", "10 - abs(y)", "1.75 / exp((x * 5)*(x * 5) * (y * 5)*(y * 5))", "pow(tem , 0.5)*0.5", "sqrt(36 - (10- sqrt(tem))*(10- sqrt(tem)))","cos(abs(x) + abs(y))","(sin(4 * x) - cos(5 * y))/5" };
+			static int listbox_item_current = 1;
+			ImGui::ListBox("Function List", &listbox_item_current, listbox_items, IM_ARRAYSIZE(listbox_items), 4);
+			if (ImGui::Button("Render"))                            // Buttons return true when clicked (NB: most widgets return true when edited/activated)
+				createFunction(listbox_item_current);
 			static float f = 0.0f;
-			static int counter = 0;
-			ImGui::Text("Hello, world!");                           // Display some text (you can use a format string too)
+
+			const char* listbox_items2[] = { "Fill", "Wire Frame", "Fill with Wire Frame"};
+			static int listbox_item_current2 = 1;
+			ImGui::ListBox("Mode Select", &listbox_item_current2, listbox_items2, IM_ARRAYSIZE(listbox_items2), 2);
+			if (ImGui::Button("Apply Mode"))                            // Buttons return true when clicked (NB: most widgets return true when edited/activated)
+				createFunction(listbox_item_current);
+			/*ImGui::Text("Hello, world!");    */                       // Display some text (you can use a format string too)
 			ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
 			ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
 
 			ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our windows open/close state
 			ImGui::Checkbox("Another Window", &show_another_window);
 
-			if (ImGui::Button("Button"))                            // Buttons return true when clicked (NB: most widgets return true when edited/activated)
-				counter++;
+			
 			ImGui::SameLine();
-			ImGui::Text("counter = %d", counter);
+			/*ImGui::Text("counter = %d", counter);*/
 
 			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
 		}
